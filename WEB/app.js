1ea8ecae12369e1f8e6fbecbf4027f64637db365
NaserAlqimlas <naal9366@colorado.edu>
2019-04-20 14:28:06 -0600

    heatmap displays latest tweets only

--- WEB/app.js
+++ WEB/app.js
@@ -32,69 +32,69 @@ mongoose.connect(
   }
 );
 
-function create() {
-  Usa.create(
-    {
-      AL: 10000000,
-      AK: 0,
-      AZ: 1,
-      AR: 0,
-      CA: 10000,
-      CO: 500,
-      CT: 0,
-      DE: 1,
-      FL: 500,
-      GA: 0,
-      HI: 1,
-      ID: 4,
-      IL: 0,
-      IN: 0,
-      IA: 0,
-      KS: 1,
-      KY: 1,
-      LA: 1000000,
-      ME: 2,
-      MD: 4,
-      MA: 12,
-      MI: 9,
-      MN: 500,
-      MS: 7,
-      MO: 80000,
-      MT: 34,
-      NE: 4,
-      NV: 6,
-      NH: 4,
-      NJ: 34,
-      NM: 2,
-      NY: 34,
-      NC: 7,
-      ND: 6,
-      OH: 54,
-      OK: 3,
-      OR: 5,
-      PA: 600,
-      RI: 5,
-      SC: 2,
-      SD: 6,
-      TN: 4,
-      TX: 23,
-      UT: 67,
-      VT: 23,
-      VA: 43,
-      WA: 54,
-      WV: 12,
-      WI: 44,
-      WY: 66
-    },
-    (error, data) => {
-      if (error) {
-        console.log(`Error Happen ${error}`);
-      } else {
-        console.log(`Data created successfully${data}`);
-      }
-    }
-  );
-}
+// function create() {
+//   Usa.create(
+//     {
+//       AL: 10000000,
+//       AK: 0,
+//       AZ: 1,
+//       AR: 0,
+//       CA: 10000,
+//       CO: 500,
+//       CT: 0,
+//       DE: 1,
+//       FL: 500,
+//       GA: 0,
+//       HI: 1,
+//       ID: 4,
+//       IL: 0,
+//       IN: 0,
+//       IA: 0,
+//       KS: 1,
+//       KY: 1,
+//       LA: 1000000,
+//       ME: 2,
+//       MD: 4,
+//       MA: 12,
+//       MI: 9,
+//       MN: 500,
+//       MS: 7,
+//       MO: 80000,
+//       MT: 34,
+//       NE: 4,
+//       NV: 6,
+//       NH: 4,
+//       NJ: 34,
+//       NM: 2,
+//       NY: 34,
+//       NC: 7,
+//       ND: 6,
+//       OH: 54,
+//       OK: 3,
+//       OR: 5,
+//       PA: 600,
+//       RI: 5,
+//       SC: 2,
+//       SD: 6,
+//       TN: 4,
+//       TX: 23,
+//       UT: 67,
+//       VT: 23,
+//       VA: 43,
+//       WA: 54,
+//       WV: 12,
+//       WI: 44,
+//       WY: 66
+//     },
+//     (error, data) => {
+//       if (error) {
+//         console.log(`Error Happen ${error}`);
+//       } else {
+//         console.log(`Data created successfully${data}`);
+//       }
+//     }
+//   );
+// }
 
 // const data = usa.find({}, (error, data) => {
 //   if(error){
@@ -117,7 +117,6 @@ app.post("/choropleth", (req, res) => {
   console.log(keyword);
   // 執行 cat child_process.js 指令：
   //May not work in Heroku
-  create();
   exec(
     "gcloud dataproc jobs submit pyspark --cluster=cluster-56f5 --region=us-west1 data_processing.py -- " +
       keyword,
@@ -127,9 +126,10 @@ app.post("/choropleth", (req, res) => {
       }
       console.info("cat child_process.js stdout: ");
       console.log(stdout);
-      setTimeout(function() {
-        res.redirect("/choropleth");
-      }, 1000);
+      // setTimeout(function() {
+      //   res.redirect("/choropleth");
+      // }, 60000);
+      res.redirect("/choropleth");
     }
   );
   //Store this data to the mongoDB database for Spark query
@@ -142,7 +142,8 @@ app.get("/us-states", (req, res) => {
 //Get the last item from MongoDB
 app.get("/bigdata", (req, res) => {
   Usa.find({}).then(users => {
-    res.send(users);
+    console.log(users.length - 1);
+    res.send([users[users.length - 1]]);
   });
 });
 // Trying to get keyword from search bar
