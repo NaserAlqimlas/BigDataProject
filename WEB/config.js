660fde7e6258819426d718c25e344d631eec4210
yamahachou <yamahachou@hotmail.com>
2019-04-22 18:01:07 -0600

    Add files via upload

---
+++ WEB/config.js
@@ -0,0 +1,11 @@
+const config = {
+  app: {
+    port: 3000
+  },
+  db: {
+    // mongodb: "mongodb://a123456:a123456@ds145828.mlab.com:45828/bigdatacu"
+    mongodb: "mongodb://104.197.54.204/tweets"
+  }
+};
+
+module.exports = config;
