5b5c1ef713c903881082b39dab107f5fdff9a776
NaserAlqimlas <naal9366@colorado.edu>
2019-04-28 17:19:22 -0600

    connecting to actual db instead of test db

--- WEB/config.js
+++ WEB/config.js
@@ -4,8 +4,8 @@ const config = {
   },
   db: {
     //mongodb: "mongodb://a123456:a123456@ds145828.mlab.com:45828/bigdatacu"
-    //mongodb: "mongodb://104.197.54.204/tweets"
-    mongodb: "mongodb://35.224.238.194:27017/testcdn"
+    mongodb: "mongodb://104.197.54.204/tweets"
+    //mongodb: "mongodb://35.224.238.194:27017/testcdn"
   }
 };
 
